2b439ed19deaa9fcc2b5dc6a6dd7b76ed8b0549a
durgaprasad-kolli <119840335+durgaprasad-kolli@users.noreply.github.com>
2023-01-22 22:32:57 +0530

    #include<stdio.h> int main() {     int r,n,a;     scanf("%d%d%d",&r,&n,&a);     int i;     for(i=n;i<=a;i++)         {         printf("%d x %d = %d ",r,i,r*i);}
    }

---
+++ Multiplication_Table_3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int r,n,a;
+    scanf("%d%d%d",&r,&n,&a);
+    int i;
+    for(i=n;i<=a;i++)
+        {
+        printf("%d x %d = %d
+",r,i,r*i);}
+    
+    
+}
