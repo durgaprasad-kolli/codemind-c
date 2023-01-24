318ef1e9f78ba98a774e1954a420832dfa96903e
durgaprasad-kolli <119840335+durgaprasad-kolli@users.noreply.github.com>
2023-01-24 11:42:56 +0530

    #include<stdio.h> int main() {     int i,m,n;     scanf("%d%d",&m,&n);   //write a progeam of even     for(i=m;i<=n;i++)     {         if(i%2==0)         printf("%d ",i);     } }

---
+++ Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,m,n;
+    scanf("%d%d",&m,&n);   //write a progeam of even
+    for(i=m;i<=n;i++)
+    {
+        if(i%2==0)
+        printf("%d ",i);
+    }
+}
