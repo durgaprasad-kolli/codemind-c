c8433d4003c5dcab10dceb530038cd5c68af5685
durgaprasad-kolli <119840335+durgaprasad-kolli@users.noreply.github.com>
2023-05-23 11:37:36 +0530

    #include<stdio.h> int main() {     int a,i,c=0,p,avrg,d=0,j,k;     scanf("%d",&a);     int x[a];     for(i=0;i<a;i++)     {         scanf("%d",&x[i]);     }     int dx=0;     for(i=0;i<a;i++)     {         if(x[i]%2!=0)         {           dx=dx+x[i];         }     }     printf("%d",dx);
    }

---
+++ Odd_Sum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,c=0,p,avrg,d=0,j,k;
+    scanf("%d",&a);
+    int x[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    int dx=0;
+    for(i=0;i<a;i++)
+    {
+        if(x[i]%2!=0)
+        {
+          dx=dx+x[i];
+        }
+    }
+    printf("%d",dx);
+    
+} 
