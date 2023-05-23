19299d3b2a585249c6b17cc0973c723a4958ec9a
durgaprasad-kolli <119840335+durgaprasad-kolli@users.noreply.github.com>
2023-05-23 11:36:35 +0530

    #include<stdio.h> int main() {     int a,i,c=0,p,avrg,d=0,j,k;     scanf("%d",&a);     int x[a];     for(i=0;i<a;i++)     {         scanf("%d",&x[i]);     }     int cx=0;      for(i=0;i<a;i++)     {         if(i%2==0){         cx=cx+x[i];}
        }
        printf("%d",cx);
       
    }

--- Sum_of_Even_Positions.c
+++ Sum_of_Even_Positions.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,s=0;
-	scanf("%d",&n);
-	int a[n] ;															
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-	    if(i%2==0)
-	    {
-
-		s=s+a[i];
-	    }
-	}
-	printf("%d",s);
-}
+    int a,i,c=0,p,avrg,d=0,j,k;
+    scanf("%d",&a);
+    int x[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    int cx=0;
+     for(i=0;i<a;i++)
+    {
+        if(i%2==0){
+        cx=cx+x[i];}
+        
+    }
+    printf("%d",cx);
+   
+} 
